e661cbdae581ab18941a552912f87430d0bd49a7
Haojian Wu <hokein.wu@gmail.com>
2014-02-20 10:30:31 +0800

    [DP] Climb stairs;

---
+++ src/climb_stairs.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+typedef unsigned long long ull;
+typedef long long ll;
+
+class Solution {
+ public:
+  int climbStairs(int n) {
+    vector<int> dp(n+1, 0);
+    dp[1] = 1;
+    dp[2] = 2;
+    for (int i = 3; i <= n; ++i)    
+      dp[i] = dp[i-1] + dp[i-2];
+    return dp[n];
+  }
+};
+
+int main() {
+  freopen("test.txt", "r", stdin);
+
+  Solution s;
+  cout << s.climbStairs(3) << endl;
+  return 0;
+} 
